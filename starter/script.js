de685bef48e0d9f7787b427d60bfca45dd07a307
Emily Grivot <emilygrivot@hotmail.co.uk>
2024-03-09 21:05:08 +0000

    created forecast blocks and current weather

--- starter/script.js
+++ starter/script.js
@@ -1,21 +1,12 @@
-// API url with API key -> https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4a727a43e8e7f8d3a04f2aa378bedb8d
-//need to get lat & lon for city
-
-const currentDay = dayjs().format("DD MMMM YYYY");
-// let cityName;
-
 // Search for a city
 $("#search-button").click(() => {
   event.preventDefault();
   let cityName = $("#search-input").val();
   searchCity(cityName);
-
-  //   console.log(cityName);
 });
 
 const searchCity = async (cityName) => {
   let cityResponse;
-  //   const coordResponse = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityResponse.lat}&lon=${cityResponse.lon}&cnt=5&appid=4a727a43e8e7f8d3a04f2aa378bedb8d`
 
   fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=4a727a43e8e7f8d3a04f2aa378bedb8d`
@@ -24,30 +15,69 @@ const searchCity = async (cityName) => {
     .then((data) => {
       cityResponse = data[0];
       fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${cityResponse.lat}&lon=${cityResponse.lon}&cnt=5&appid=4a727a43e8e7f8d3a04f2aa378bedb8d`
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${cityResponse.lat}&lon=${cityResponse.lon}&exclude=hourly,minutely&appid=4a727a43e8e7f8d3a04f2aa378bedb8d&units=metric`
       )
         .then((response) => response.json())
         .then((data) => {
-          currentWeather(data);
+          currentWeather(data.current, cityName);
+          dailyForecast(data.daily.slice(1, 6));
         });
     });
 };
+// validation + catch errors
+// local storage for recent searches, create buttons
+// styling
 
-// function to get current day weather data
-const currentWeather = (data) => {
-  var myDate = data.dt * 1000;
-  console.log(myDate);
+const currentWeather = (data, cityName) => {
+  const date = dayjs.unix(data.dt).format(`D[/]MM[/]YYYY`);
+
   var weatherToday =
-    "<div id='today'>" + "<h1>" + data.name + " " + myDate + "</h1>" + "</div>";
-  //temp
-  //wind
-  //humidity
+    "<div id='today'>" +
+    "<h3>" +
+    cityName +
+    " " +
+    date +
+    "</h3>" +
+    "<p> Temp: " +
+    data.temp +
+    " " +
+    "Celsius" +
+    "<p> Humidity: " +
+    data.humidity +
+    "%" +
+    "<p> Wind speed: " +
+    data.wind_speed +
+    " km/h" +
+    "</div>";
   $("#today").append(weatherToday);
 };
 
 // function to get 5 day forecast
 
-const forecast = () => {};
+const dailyForecast = (array) => {
+  array.forEach((forecast, i) => {
+    const date = dayjs.unix(forecast.dt).format(`D[/]MM[/]YYYY`);
+    var dailyWeather =
+      "<div id='daily-" +
+      i +
+      "'>" +
+      "<h4> " +
+      date +
+      "</h4>" +
+      "<p> Temp: " +
+      forecast.temp.day +
+      " " +
+      "Celsius" +
+      "<p> Humidity: " +
+      forecast.humidity +
+      "%" +
+      "<p> Wind speed: " +
+      forecast.wind_speed +
+      " km/h" +
+      "</div>";
+    $("#forecast").append(dailyWeather);
+  });
+};
 
 // create array of previously searched cities
 const topCities = [];
